05ea369370f75e4994c63d3794fb4c7c3d657dc0
Luizefz <luizefsilva2@gmail.com>
2023-04-26 21:04:08 -0300

    feat: path com Interface - R.N

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
         nome={time.nome}
         corPrimaria={time.corPrimaria}
         corSecundaria={time.corSecundaria}
-        colaborador={colaboradores.filter(colaborador => colaborador.time === time.nome)} />)}
+        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} />)}
 
       <Rodape />
     </div>
---
+++ src/components/Time/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Time.css'
+import { IColaborador } from '../../shared/interfaces/iColaborador'
+import Colaborador from '../Colaborador'
+
+interface TimeProps {
+    corPrimaria: string
+    corSecundaria: string
+    nome: string
+    colaboradores: IColaborador[]
+}
+
+const Time = (props: TimeProps) => {
+    return (
+        (props.colaboradores.length) ?
+            <section className='time' style={{ backgroundColor: props.corSecundaria }}>
+                <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+                <div className='colaboradores'>
+                    {props.colaboradores.map(colaborador =>
+                        <Colaborador
+                            key={colaborador.nome}
+                            corDeFundo={props.corPrimaria}
+                            nome={colaborador.nome}
+                            imagem={colaborador.imagem}
+                            cargo={colaborador.cargo}
+                        />
+                    )}
+                </div>
+            </section>
+            : ' '
+    )
+}
+
+export default Time
